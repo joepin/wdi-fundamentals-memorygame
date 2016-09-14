f8406b3d2e2fbfe7cd9235c25d8b4741a0e0f7db
Joey Pinhas <Joey Pinhas>
2016-09-14 15:53:45 -0400

    created a function to populate the board div with 4 card divs; commented out game logic from previous commit. also commented out the hard-coded card divs from initial commit.

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,19 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardFour){
-	alert ("You found a match!");
-} else {
-	alert ("Sorry, try again.");
+// if (cardOne === cardFour){
+// 	alert ("You found a match!");
+// } else {
+// 	alert ("Sorry, try again.");
+// }
+
+function createBoard(){
+	var gameBoard = document.getElementById("game-board");
+	for (var i = 0; i < 4; i++){
+		var newCard = document.createElement('div');
+		newCard.className = "card";
+		gameBoard.appendChild(newCard);
+	}
 }
+
+createBoard();
