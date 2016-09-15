b65da48327c73d7e908702d9fa0b4567c0161758
Joey Pinhas <Joey Pinhas>
2016-09-15 11:12:41 -0400

    Added comments to code. Version 1 is complete.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,16 @@
+//array of desired card values
 var cards = ["queen", "queen", "king", "king"];
+//global array to hold user's selected cards
 var cardsInPlay = [];
+//string variables to hold HTML for different card face images
 var queenImage = '<img src="queen.png" alt="Queen of Hearts" width="100%" height="100%">';
 var kingImage = '<img src="king.png" alt="King of Hearts" width="100%" height="100%">';
 
+/*
+This function is essenttially the initializing function for our game.
+It should only be called once - by the main flow of our script.
+This function creates as many cards as are in the "cards" array defined above. 
+*/
 function createBoard(){
 	var gameBoard = document.getElementById("game-board");
 	for (var i = 0; i < cards.length; i++){
@@ -14,6 +22,11 @@ function createBoard(){
 	}
 }
 
+/*
+This function is attached to each card's click event, and is the main controller of the game.
+It first adds the current card to the cardsInPlay array, calls setCardFace on this card, then checks if the array has two cards.
+If it does, this function calls isMatch. Otherwise, it does nothing more.
+*/
 function isTwoCards(){
 	cardsInPlay.push(this);
 	setCardFace(this);
@@ -23,6 +36,11 @@ function isTwoCards(){
 	}
 }
 
+/*
+This function determines if the cards that the user selected are a match.
+Currently, it utilizes the alert method to provide feedback to the user.
+Regardless of the outcome, this function calls clearCards to reset the card faces to nothing.
+*/
 function isMatch(cardsInPlay){
 	if (cardsInPlay[0].getAttribute('data-card') === cardsInPlay[1].getAttribute('data-card')){
 		alert ("You found a match!");
@@ -32,6 +50,10 @@ function isMatch(cardsInPlay){
 	clearCards(cardsInPlay);
 }
 
+/*
+This function takes in a card object as a parameter and sets its HTML to the appropriate card face image.
+It is called every time the user selects a card.
+*/
 function setCardFace(card) {
 	var cardVal = card.getAttribute('data-card');
 	var toEnter = null;
@@ -48,10 +70,15 @@ function setCardFace(card) {
 	card.innerHTML = toEnter;
 }
 
+/*
+This function simply iterates over the cardsInPlay array to set each element's innerHTML to null.
+By doing so, we reset the card's faces.
+*/
 function clearCards(cardsInPlay) {
 	for (var i = 0; i < cardsInPlay.length; i++){
 		cardsInPlay[i].innerHTML = '';
 	}
 }
 
+//call the createBoard function to finish setting up the DOM
 createBoard();
